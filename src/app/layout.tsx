4a329a7b58a7edcc0af8effb619c5e99a9ef9e6c
Giorgi Gagoshidze <g.gagoshidze@omedia.ge>
2024-12-29 19:10:17 +0400

    fix fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
-import { Header } from "@/components/layout/header";
+import { Header } from "@/components/layout/header-comp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
